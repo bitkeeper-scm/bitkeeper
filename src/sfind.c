6dd32c36e6de4170bff78b0f250ac6b1a4e9a2e3
Rick Smith <rick@bitkeeper.com>
1999-08-27 15:39:01 -0700

    sfind.c 	removed delta which looked at cset marks instead of ChangeSet file sfind.c 	contents.  Added in condition to not check ChangeSet file for not sfind.c 	being in ChangeSet (they always are, and no longer stored in delta sfind.c 	body).
    bk: 37c71385KkWPoVjT7LWBDuGDsjEHmw

--- src/sfind.c
+++ src/sfind.c
@@ -327,7 +327,6 @@ visit(delta *d)
 	if (d->parent) visit(d->parent);
 }
 
-#ifdef	CRAZY_WOW
 /*
  * Print out everything leading from start to d, not including start.
  * XXX - stolen from cset.c - both copies need to go in slib.c.
@@ -354,7 +353,6 @@ csetDeltas(sccs *sc, delta *start, delta *d)
 	// Is this an issue?  I think makepatch handles them.
 	if (d->type == 'D') printf("%s:%s\n", sc->sfile, d->rev);
 }
-#endif
 
 save(sccs *sc, MDBM *idDB, char *buf)
 {
@@ -411,7 +409,7 @@ caches(const char *filename, const struct stat *sb, int flag)
 		}
 	}
 
-	unless (Cflg) {
+	unless (Cflg && !(sc->state & S_CSET)) {
 		sccs_free(sc);
 		return (0);
 	}
@@ -423,21 +421,6 @@ caches(const char *filename, const struct stat *sb, int flag)
 		return (0);
 	}
 
-	/*
-	 * If it's marked, we're done.
-	 */
-	if (d->flags & D_CSET) {
-		sccs_free(sc);
-		return;
-	}
-
-	printf("%s:%s\n", sc->sfile, d->rev);
-	while (aFlg && d->parent && !(d->parent->flags & D_CSET)) {
-		d = d->parent;
-		printf("%s:%s\n", sc->sfile, d->rev);
-	}
-
-#ifdef	CRAZY_WOW
 	/* Go look for long and short versions in the cset */
 	sccs_sdelta(sc, sccs_ino(sc), buf);
 	unless (t = mdbm_fetch_str(csetDB, buf)) {
@@ -465,7 +448,6 @@ caches(const char *filename, const struct stat *sb, int flag)
 			printf("%s:%s\n", sc->sfile, d->rev);
 		}
 	}
-#endif
 	sccs_free(sc);
 	return (0);
 }
