746863fba4efa1d4eac83c1f2e0aa3892be4e8d7
Larry McVoy <lm@bitkeeper.com>
1999-09-17 12:08:07 -0700

    Makefile	Make a PRIVATE target for binaries which we do not install. Makefile	Add ndebug target for perf testing. resync.perl	Lint ISSUES  	Update. t.delta 	touch issues mtime.c 	BitKeeper file /home/lm/smt/bk/src/mtime.c
    bk: 37e29197_pZA8GD4beKsfjmUqKLj5Q

---
+++ src/mtime.c
@@ -0,0 +1,36 @@
+/*
+ * Stat the file and print out the time as YYYY-MM-DD-HH-MM-SS
+ */
+#include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+main(int ac, char **av)
+{
+	struct	stat st;
+	struct	tm *t;
+
+	if (ac != 2) {
+		fprintf(stderr, "usage: %s file\n", av[0]);
+		exit(1);
+	}
+	if (stat(av[1], &st)) {
+		perror(av[1]);
+		exit(2);
+	}
+
+	/*
+	 * GNU's touch seems to set things in local time.
+	 * We'll see if this is portable.
+	 */
+	t = localtime(&st.st_mtime);
+	printf("%d-%02d-%02d-%02d:%02d:%02d\n",
+	    t->tm_year + 1900,
+	    t->tm_mon + 1, 
+	    t->tm_mday,
+	    t->tm_hour,
+	    t->tm_min,
+	    t->tm_sec);
+	exit(0);
+}
