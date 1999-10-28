ba1d33ba51164860826f3b6fc8feb06ebfbf5994
Larry McVoy <lm@bitkeeper.com>
1999-10-27 23:46:28 -0700

    Misc, includes fm3 update.
    bk: 3817f144JbjaUoN4AvNfIodiWbsAnw

--- src/bk.sh
+++ src/bk.sh
@@ -693,7 +693,7 @@ _undo() {
 	then	echo undo: nothing to undo in "$1"
 		exit 0
 	fi
-	sed 's/[:@].*$//' < ${TMP}rmlist$$ | bk clean - || {
+	sed 's/[:@].*$//' < ${TMP}rmlist$$ | ${BIN}clean - || {
 		echo Undo aborted.
 		$RM -f ${TMP}rmlist$$
 		exit 1
@@ -1281,11 +1281,18 @@ _export() {
 	_cd2root
 
 	# XXX: cset -t+ should work.
-	(${BIN}cset -t`${BIN}prs $R -hd:I: ChangeSet`) \
+	CREV=`${BIN}prs $R -hd:I: ChangeSet`
+	if [ X$CREV = X ]
+	then	echo "export: unable to find revision $R in ChangeSet"
+		rmdir $DST
+		exit 1
+	fi
+	
+	${BIN}cset -t`${BIN}prs $R -hd:I: ChangeSet` \
 	| eval egrep -v "'^(BitKeeper|ChangeSet)'" $INCLUDE $EXCLUDE \
 	| sed 's/:/ /' | while read file rev
 	do
-		PN=`bk prs -r$rev -hd:DPN: $SRC/$file`
+		PN=`${BIN}prs -r$rev -hd:DPN: $SRC/$file`
 		if ! ${BIN}get $K $Q -r$rev -G$DST/$PN $SRC/$file
 		then	DIR=`dirname $DST/$$PN`
 			mkdir -p $DIR || exit 1
