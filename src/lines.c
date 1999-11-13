3d49c84489cec6d83fae22a6b340f15a2cb0d458
Larry McVoy <lm@bitkeeper.com>
1999-11-13 03:40:08 -0800

    lines.c lod support.
    bk: 382d4e18kOTc8CV1VWGXWxZxWeuagw

--- src/lines.c
+++ src/lines.c
@@ -3,9 +3,6 @@
 WHATSTR("@(#)%K%");
 
 void	prevs(sccs *d);
-delta	*trunk(delta *d);
-void	locations(delta *d, int deleted, int added, int markit);
-int	cmp(delta **a, delta **b);
 void	branches(delta *d);
 void	p(delta *d);
 int	flags;
@@ -36,65 +33,6 @@ main(int ac, char **av)
 	return (0);
 }
 
-
-/*
- * Return the next delta on the trunk or NULL.
- */
-delta *
-trunk(delta *d)
-{
-	if (!d->kid || (d->kid->type == 'R')) return (0);
-	d = d->kid;
-	if (d->r[2]) return (0);
-	return (d);
-}
-
-/*
- * figure out the location of each revision.
- * depth is distance to the root and
- * width is distance from the trunk.
- */
-#define	depth	deleted
-#define	width	added
-void
-locations(delta *d, int depth, int width, int markit)
-{
-	if (markit && (d->type == 'D')) {
-		d->depth = depth;
-		d->width = width;
-	} else {
-		d->depth = 0;
-		d->width = 0;
-	}
-	if (d->kid) {
-		if (d->kid->type == 'R') {
-			locations(d->kid, depth, width, 0);
-		} else {
-			locations(d->kid, depth+1, width, 0);
-		}
-	}
-	if (d->siblings) {
-		if (d->siblings->type == 'R') {
-			locations(d->siblings, depth, width, 1);
-		} else {
-			locations(d->siblings, depth, width+1, 1);
-		}
-	}
-}
-
-int
-cmp(delta **a, delta **b)
-{
-	delta	*e = *a;
-	delta	*d = *b;
-	int	l = (d->r[1] << 16) + (65535 - d->r[2]);
-	int	r = (e->r[1] << 16) + (65535 - e->r[2]);
-
-	return (l - r);
-	if (d->r[2] > e->r[2]) return (d->r[2] - e->r[2]);
-	return (d->r[3] - e->r[3]);
-}
-
 /*
  * First, find the leaf delta which is on the trunk and list that.
  * Then walk all the leaves and print them out.
@@ -117,7 +55,8 @@ void
 branches(delta *d)
 {
 	if (!d) return;
-	if ((d->r[3] == 1) && (d->type != 'R') && !(d->flags & D_VISITED)) {
+	if (((d->r[3] == 1) || ((d->r[1] == 1) && !d->r[2])) &&
+	    (d->type != 'R') && !(d->flags & D_VISITED)) {
 		p(d);
 	}
 	branches(d->kid);
