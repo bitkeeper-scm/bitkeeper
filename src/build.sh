6a7d4484fe19e044ccd81650a1c87fb99dab9880
Larry McVoy <lm@bitkeeper.com>
1999-12-16 20:29:52 -0800

    Build updates.
    bk: 3859bc40Tf2xoX5Kg9tM6V-gOwQHcQ

--- src/build.sh
+++ src/build.sh
@@ -1,28 +1,23 @@
+#!/bin/sh
+
 HOST=work
 TREE=prerelease
+USER=`bk getuser`
+REPO=$TREE-$USER
 CC=gcc
 MAKE=gmake
-PRSH="PREFER_RSH=YES"
-DIR=/tmp
 PATH=/usr/local/bin:/usr/freeware/bin:/usr/ccs/bin:$PATH
 export PATH
-
 case `uname -s` in
-	SunOS)
-		XLIBS="XLIBS=-lnsl"
-	;;
-	AIX)
-		MAKE=make
-		DIR=/usr/tmp
-	;;
+	SunOS)	XLIBS="XLIBS=-lnsl"
+		;;
+	AIX)	MAKE=make
+		;;
 esac
 
-oldDIR=`pwd`
-cd $DIR
-test -d build || mkdir build
-cd build
-rm -rf $TREE
+cd /tmp || exit 1
+rm -rf $REPO
 set -e
-eval $PRSH bk clone $HOST:/home/bk/$TREE $TREE > LOG 2>&1
-cd $TREE/src
-time $MAKE CC=$CC $XLIBS production >> ../../LOG 2>&1
+PREFER_RSH=YES bk clone $HOST:/home/bk/$TREE $REPO > LOG-$USER 2>&1
+cd $REPO/src
+time $MAKE CC=$CC $XLIBS production >> ../../LOG-$USER 2>&1
