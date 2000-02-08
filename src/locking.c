926f82490b1996ec783836bbf93baac9ef8257d0
Larry McVoy <lm@bitkeeper.com>
2000-02-07 20:53:04 -0800

    Another attempt to keep the locks from getting stale.
    bk: 389fa130GcFEg3MKctmhuGY3SKdW2A

--- src/locking.c
+++ src/locking.c
@@ -30,6 +30,84 @@ repository_lockers(project *p)
 }
 
 /*
+ * Try to unlock a project.
+ * Currently only handles readers, not writers.
+ * Returns 0 if successful.
+ */
+int
+repository_unlock(project *p)
+{
+	char	*root;
+	char	path[MAXPATH];
+	char	*host;
+	u32	pid;
+	int	freeit = 0;
+	int	left = 0;
+	DIR	*D;
+	struct	dirent *d;
+	int	flags = 0;
+
+	unless (p) return (-1);
+	if (p->flags & PROJ_WRLOCK) return (-1);
+
+	if (exists("BitKeeper/etc")) {
+		root = ".";
+	} else {
+		if (p->root) {
+			sprintf(path, "%s/BitKeeper/etc", p->root);
+			if (exists(path)) {
+				root = p->root;
+			} else {
+				root = sccs_root(0);
+				freeit = 1;
+			}
+		} else {
+			root = sccs_root(0);
+			freeit = 1;
+		}
+	}
+	unless (root) return (-1);
+	
+	unless (host = sccs_gethost()) {
+		fprintf(stderr, "bk: can't figure out my hostname\n");
+		if (freeit) free(root);
+		return (-1);
+	}
+
+	sprintf(path, "%s/%s", root, READER_LOCK_DIR);
+	unless (exists(path)) {
+		verbose((stderr, "bk: can not find lock directory\n"));
+		if (freeit) free(root);
+		return (-1);
+	}
+
+	unless (D = opendir(path)) {
+		perror(path);
+		if (freeit) free(root);
+		return (-1);
+	}
+	while (d = readdir(D)) {
+		if (streq(d->d_name, ".") || streq(d->d_name, "..")) continue;
+		pid = 0;
+		path[0] = 0;
+		sscanf(d->d_name, "%d@%s", &pid, path);
+		if (streq(path, host) &&
+		    (kill((pid_t)pid, 0) != 0) && (errno == ESRCH)) {
+		    	verbose((stderr, "bk: discarding stale lock %s\n",
+			    d->d_name));
+			sprintf(path,
+			    "%s/%s/%s", root, READER_LOCK_DIR, d->d_name);
+			unlink(path);
+		} else {
+			left++;
+		}
+	}
+	closedir(D);
+	if (freeit) free(root);
+	return (left ? -1 : 0);
+}
+
+/*
  * Try and get a read lock for the whole repository.
  * Return -1 if failed, 0 if it worked.
  */
@@ -72,88 +150,14 @@ int
 repository_rdunlock()
 {
 	char	path[MAXPATH];
+	project	*p;
 
 	unless (exists("BitKeeper/etc")) return(-1);
 
 	sprintf(path, "%s/%d@%s", READER_LOCK_DIR, getpid(), sccs_gethost());
 	unlink(path);
+	p = sccs_initProject(0);
+	repository_unlock(p);
+	sccs_freeProject(p);
 	return (0);
-}
-
-/*
- * Try to unlock a project.
- * Currently only handles readers, not writers.
- * Returns 0 if successful.
- */
-int
-repository_unlock(project *p)
-{
-	char	*root;
-	char	path[MAXPATH];
-	char	*host;
-	u32	pid;
-	int	freeit = 0;
-	int	left = 0;
-	DIR	*D;
-	struct	dirent *d;
-
-
-	unless (p) return (-1);
-	if (p->flags & PROJ_WRLOCK) return (-1);
-
-	if (exists("BitKeeper/etc")) {
-		root = ".";
-	} else {
-		if (p->root) {
-			sprintf(path, "%s/BitKeeper/etc", p->root);
-			if (exists(path)) {
-				root = p->root;
-			} else {
-				root = sccs_root(0);
-				freeit = 1;
-			}
-		} else {
-			root = sccs_root(0);
-			freeit = 1;
-		}
-	}
-	unless (root) return (-1);
-	
-	unless (host = sccs_gethost()) {
-		fprintf(stderr, "bk: can't figure out my hostname\n");
-		if (freeit) free(root);
-		return (-1);
-	}
-
-	sprintf(path, "%s/%s", root, READER_LOCK_DIR);
-	unless (exists(path)) {
-		fprintf(stderr, "bk: can not find lock directory\n");
-		if (freeit) free(root);
-		return (-1);
-	}
-
-	unless (D = opendir(path)) {
-		perror(path);
-		if (freeit) free(root);
-		return (-1);
-	}
-	while (d = readdir(D)) {
-		if (streq(d->d_name, ".") || streq(d->d_name, "..")) continue;
-		pid = 0;
-		path[0] = 0;
-		sscanf(d->d_name, "%d@%s", &pid, path);
-		if (streq(path, host) &&
-		    (kill((pid_t)pid, 0) != 0) && (errno == ESRCH)) {
-		    	fprintf(stderr, "bk: discarding stale lock %s\n",
-			    d->d_name);
-			sprintf(path,
-			    "%s/%s/%s", root, READER_LOCK_DIR, d->d_name);
-			unlink(path);
-		} else {
-			left++;
-		}
-	}
-	closedir(D);
-	if (freeit) free(root);
-	return (left ? -1 : 0);
 }
