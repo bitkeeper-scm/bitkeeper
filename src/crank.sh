34cd7c384d594426a8e8657fa71621d751815d00
Larry McVoy <lm@bitkeeper.com>
2000-02-12 22:41:39 -0800

    crankturn fix so it works for trees other than prerelease.
    bk: 38a652232uqEEVrH6tvz3bHZ2UTbNg

--- src/crank.sh
+++ src/crank.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-HOST=work
-if [ X$1 != X ]
-then	TREE=$1
-else	TREE=prerelease
-fi
+exec > /tmp/LOG-$USER 2>&1
+set -x
+
+# We start in the src subdir
+cd ..
+TREE=`pwd`
+TREE_HOST=work
 USER=`bk getuser`
-REPO=$TREE-$USER
+REPO=`basename $TREE`-$USER
 cd /tmp || exit 1
 rm -rf $REPO
 set -e
-exec > LOG-$USER 2>&1
-PREFER_RSH=YES bk clone $HOST:/home/bk/$TREE $REPO
+PREFER_RSH=YES bk clone $TREE_HOST:$TREE $REPO
 cd $REPO/src
 get build.sh || exit 1
 cp build.sh build
