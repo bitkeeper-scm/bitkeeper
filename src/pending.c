f0c0bd09aa83478fc310c1fb5db2ea1787b3a703
Andrew Chang <awc@bitkeeper.com>
2000-06-04 13:32:02 -0700

    win32: fix a hang problem when we do a pull. The hang is in diff3.exe caused by a pipe constructed by sh.exe.
    bk: 393abcc2oYFwP-6c1w4oxlMPcBQF1g

--- src/pending.c
+++ src/pending.c
@@ -6,7 +6,8 @@ extern char *pager;
 int
 pending_main(int ac, char **av)
 {
-	char	buf[MAXLINE];
+	int 	rc;
+	char	buf[MAXLINE], *p;
 	char	*dspec =
 ":DPN:@:I:, :Dy:-:Dm:-:Dd: :T::TZ:, :P:$if(:HT:){@:HT:}\\n\
 $each(:C:){  (:C:)} \\n$each(:SYMBOL:){  TAG: (:SYMBOL:)\\n}";
@@ -15,7 +16,19 @@ $each(:C:){  (:C:)} \\n$each(:SYMBOL:){  TAG: (:SYMBOL:)\\n}";
 		fprintf(stderr, "pending: can not find project root\n");
 		exit(1);
 	}
+#ifdef WIN32
+	sprintf(buf, "BK_YEAR4=1");
+	putenv(p=strdup(buf));
+	sprintf(buf, "bk sfiles -CA | bk prs -h '-d%s' - | %s",
+								dspec, pager);
+	rc = system(buf);
+	sprintf(buf, "BK_YEAR4=");
+	putenv(buf);
+	free(p);
+	return (rc);
+#else
 	sprintf(buf, "bk sfiles -CA | BK_YEAR4=1 bk prs -h '-d%s' - | %s",
 								dspec, pager);
 	return (system(buf));
+#endif
 }
