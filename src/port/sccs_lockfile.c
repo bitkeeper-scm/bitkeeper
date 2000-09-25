3ac8aa3eafeafea102000ce1b4a5d9b132005f48
Andrew Chang <awc@bitkeeper.com>
2000-09-25 13:12:20 -0700

    port new sccs_lockfile() code to win32.
    bk: 39cfb1a4R4PfpXaR8fIp6GdjS7y4rw

---
+++ src/port/sccs_lockfile.c
@@ -0,0 +1,76 @@
+#include "../system.h"
+#include "../sccs.h"
+
+/*
+ * Create a file with a unique name,
+ * try and link it to the lock file,
+ * if the link worked and our link count == 2, we won.
+ * If tries is set, try that many times, otherwise try forever.
+ *
+ */
+#ifdef WIN32
+int
+sccs_lockfile(char *lockfile, int tries)
+{
+	char	*s;
+	char	path[MAXPATH];
+	struct	stat sb;
+	int	slp = 1;
+	static	int uniq;
+
+	for ( ;; ) {
+		//XXX TODO: make sure this works on network drive
+		if ((close(open(lockfile,
+			O_CREAT|O_WRONLY|O_EXCL, GROUP_MODE))) == 0) return (0);
+		
+		if (tries && (tries-- == 0)) {
+			fprintf(stderr, "timed out waiting for %s\n", lockfile);
+			return (-1);
+		}
+		sleep(slp);
+		if (slp < 60) slp <<= 1;
+	}
+}
+
+
+#else
+int
+sccs_lockfile(char *lockfile, int tries)
+{
+	char	*s;
+	char	path[MAXPATH];
+	struct	stat sb;
+	int	slp = 1;
+	static	int uniq;
+
+	if (s = strrchr(lockfile, '/')) {
+		char	buf[MAXPATH];
+
+		strcpy(buf, lockfile);
+		s = strrchr(buf, '/');
+		*s = 0;
+		sprintf(path,
+		    "%s/%s%d%d", buf, sccs_gethost(), uniq++, getpid());
+	} else {
+		sprintf(path, "%s%d%d", sccs_gethost(), uniq++, getpid());
+	}
+	if (close(creat(path, GROUP_MODE))) {
+		perror(path);
+		return (-1);
+	}
+	for ( ;; ) {
+		if ((link(path, lockfile) == 0) && 
+		    (stat(path, &sb) == 0) && (sb.st_nlink == 2)) {
+		    	unlink(path);
+			return (0);
+		}
+		if (tries && (tries-- == 0)) {
+			fprintf(stderr, "timed out waiting for %s\n", lockfile);
+			unlink(path);
+			return (-1);
+		}
+		sleep(slp);
+		if (slp < 60) slp <<= 1;
+	}
+}
+#endif
