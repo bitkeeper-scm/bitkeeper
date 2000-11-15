de5e41c553af8ecc7cef7f10a7fc030c7b09a7f2
Andrew Chang <awc@bitkeeper.com>
2000-11-15 17:00:22 +0000

    Do not checks for pending logs when we are in the resync tree.
    bk: 3a12c126qha3YeLvXurvBqWUlh3pbw

--- src/bkd.h
+++ src/bkd.h
@@ -103,7 +103,7 @@ int	outfd(int fd, char*buf);
 
 int	read_blk(remote *r, char *c, int len);
 int	write_blk(remote *r, char *c, int len);
-int	logs_pending(int resync, int ptype);
+int	logs_pending(int ptype);
 remote	*remote_parse(char *p, int is_clone);
 sccs *	mk_probekey(FILE *f);
 void	sccs_color(sccs *s, delta *d);
