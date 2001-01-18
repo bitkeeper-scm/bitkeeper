8568a3d7bac5b49cc42fae7d3f28c466e07167cc
Larry McVoy <lm@bitkeeper.com>
2001-01-17 19:38:48 -0800

    Add some BSD "sccs" compat interfaces to make it easier to alias sccs=bk. Interfaces added are add,create,deledit,delget,enter,sccsdiff,val.
    bk: 3a666548ws4f6hfihSovDqFQx3aF3g

--- src/bk.c
+++ src/bk.c
@@ -38,12 +38,15 @@ int chksum_main(int, char **);
 int clean_main(int, char **);
 int cleanpath_main(int, char **);
 int clone_main(int, char **);
+int create_main(int, char **);
 int commit_main(int, char **);
 int config_main(int, char **);
 int _createlod_main(int, char **);
 int createlod_main(int, char **);
 int cset_main(int, char **);
 int delta_main(int, char **);
+int deledit_main(int, char **);
+int delget_main(int, char **);
 int diffs_main(int, char **);
 int export_main(int, char **);
 int fdiff_main(int, char **);
@@ -140,6 +143,7 @@ int unpark_main(int, char **);
 int unpull_main(int, char **);
 int unwrap_main(int, char **);
 int users_main(int, char **);
+int val_main(int, char **);
 int version_main(int, char **);
 int what_main(int, char **);
 int zone_main(int, char **);
@@ -175,6 +179,7 @@ struct command cmdtbl[] = {
 	{"_sortmerge", sortmerge_main},
 	{"_unlink", unlink_main },
 	{"abort", abort_main},		/* doc 2.0 */	
+	{"add", delta_main},		
 	{"admin", admin_main},		/* doc 2.0 */
 	{"annotate", annotate_main},	/* doc 2.0 */
 	{"bkd", bkd_main },		/* doc 2.0 */
@@ -189,10 +194,14 @@ struct command cmdtbl[] = {
 	{"commit", commit_main},	/* doc 2.0 */
 	{"config", config_main},	/* doc 2.0 */
 	{"createlod", createlod_main},	/* undoc? 2.0 */
+	{"create", create_main},	
 	{"cset", cset_main},		/* doc 2.0 */
 	{"delta", delta_main},		/* doc 2.0 */
+	{"deledit", deledit_main},	
+	{"delget", delget_main},	
 	{"diffs", diffs_main},		/* doc 2.0 */
 	{"edit", get_main},		/* aliases */	/* doc 2.0 */
+	{"enter", delta_main},
 	{"export", export_main},	/* doc 2.0 */
 	{"fdiff", fdiff_main},		/* undoc? 2.0 */
 	{"fix", fix_main},		/* doc 2.0 */
@@ -249,6 +258,7 @@ struct command cmdtbl[] = {
 	{"rmdel", rmdel_main},		/* doc 2.0 */
 	{"sane", sane_main},		/* doc 2.0 */
 	{"sccscat", sccscat_main},	/* doc 2.0 as annotate */
+	{"sccsdiff", diffs_main},
 	{"sccslog", sccslog_main},	/* doc 2.0 */
 	{"sccsmv", mv_main},		/* alias */	/* doc 2.0 as mv */
 	{"sccsrm", rm_main},		/* alias */	/* doc 2.0 as mv */
@@ -272,6 +282,7 @@ struct command cmdtbl[] = {
 	{"unpull", unpull_main},
 	{"unwrap", unwrap_main},	/* doc 2.0 */
 	{"users", users_main},		/* doc 2.0 */
+	{"val", val_main},	
 	{"version", version_main},	/* doc 2.0 */
 	{"what", what_main},		/* doc 2.0 */
 	{"zone", zone_main},		/* doc 2.0 */
