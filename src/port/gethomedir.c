3e12678defb1250ab4cbbf447c6afa9587c2d74e
Andrew Chang <awc@bitkeeper.com>
2001-03-16 17:22:13 -0800

    On win32, always compute the home directory via the registry. This make it consistent with our tcl gui.
    bk: 3ab2bc45mNXW40xdtJaF8i-ZXogRZA

--- src/port/gethomedir.c
+++ src/port/gethomedir.c
@@ -14,19 +14,12 @@ getHomeDir()
 	int	len = MAXPATH;
 #define KEY "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders"
 
-        homeDir = getenv("HOME");
-        if (homeDir == NULL) {
-		if (!getReg(KEY, "AppData", home_buf, &len)) return (NULL);
-		GetShortPathName(home_buf, tmp, MAXPATH);
-		localName2bkName(home_buf, home_buf);
-		concat_path(tmp, tmp, "BitKeeper");
-		unless (exists(tmp)) mkdir(tmp, 0640);
-                homeDir = strdup(tmp);
-        } else {
-		GetShortPathName(homeDir, tmp, MAXPATH);
-		homeDir = strdup(tmp);
-		localName2bkName(homeDir, homeDir);
-	}
+	if (!getReg(KEY, "AppData", home_buf, &len)) return (NULL);
+	GetShortPathName(home_buf, tmp, MAXPATH);
+	localName2bkName(home_buf, home_buf);
+	concat_path(tmp, tmp, "BitKeeper");
+	unless (exists(tmp)) mkdir(tmp, 0640);
+	homeDir = strdup(tmp);
         return homeDir;
 }
 #else
