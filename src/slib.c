1c86da8c41ab8b038861a68791c6b3e41f80f520
Wayne Scott <wscott@bitkeeper.com>
2001-10-16 23:05:13 -0500

    Reenable local diff code in libdiff.c and fix code to pass regressions with that diff.  The code corrently generate output slightly better than GNU diff, but it is slower and uses more memory.  But avoiding the fork/exec still makes it a win. And now we have a platform the play with diff ideas.
    bk: 3bcd0379_tYQftA7NLuurI7_OQFA0Q

--- src/slib.c
+++ src/slib.c
@@ -8606,13 +8606,26 @@ fix_cntl_a(sccs *s, char *buf, FILE *out)
 	if (buf[0] == '\001') fputdata(s, cntlA_escape, out);
 }
 
+/*
+ * Read the output of diff and determine if the files 
+ * are binary.
+ */
 private int
-nullCheck(MMAP *m)
+binaryCheck(MMAP *m)
 {
-	register u8 *p, *end;
+	u8	*p, *end;
+	int	cnt = 0;
 
 	for (p = m->mmap, end = m->end; p < end; p++) {
-		unless (*p) return (1);
+		cnt++;
+		if (*p == 0) return (1);  /* no nulls */
+		if (cnt == 1) {
+			/* GNU diff reports binary files */
+			if (strneq("Binary files ", p, 13)) return (1);
+		}
+		if (*p == '\n') {
+			cnt = 0;
+		}
 	}
 	return (0);
 }
@@ -8694,7 +8707,7 @@ out:		sccs_unlock(s, 'z');
 		return (-1);
 	}
 	if (diffs) {
-		if (nullCheck(diffs)) {
+		if (binaryCheck(diffs)) {
 			fprintf(stderr,
 			    "%s has nulls (\\0) in diffs, checkin aborted.\n",
 			    s->gfile);
@@ -10794,18 +10807,6 @@ bad:		fprintf(stderr, "bad diffs: '%.*s'\n", linelen(buf), buf);
 	return (0);
 }
 
-/*
- * The s.file should be an ascii file and the gfile should be binary.
- */
-private void
-binaryMismatch(sccs *s)
-{
-	assert(!(s->encoding & E_BINARY));
-	fprintf(stderr,
-	    "%s: file format is ascii, delta is binary.", s->sfile);
-	fprintf(stderr, "  Unsupported operation.\n");
-}
-
 int
 delta_body(sccs *s, delta *n, MMAP *diffs, FILE *out, int *ap, int *dp, int *up)
 {
@@ -10818,9 +10819,11 @@ delta_body(sccs *s, delta *n, MMAP *diffs, FILE *out, int *ap, int *dp, int *up)
 	char	*b;
 	int	no_lf = 0;
 
-	if (nullCheck(diffs)) {
+	if (binaryCheck(diffs)) {
+		assert(!(s->encoding & E_BINARY));
 		fprintf(stderr,
-		    "%s has nulls (\\0) in diffs, delta aborted.\n", s->gfile);
+		    "%s: file format is ascii, delta is binary.", s->sfile);
+		fprintf(stderr, "  Unsupported operation.\n");
 		return (-1);
 	}
 	assert(!READ_ONLY(s));
@@ -10847,12 +10850,8 @@ newcmd:
 		if (scandiff(b, &where, &what, &howmany) != 0) {
 			int	len = linelen(b);
 
-			if ((len > 13) && strneq("Binary files ", b, 13)) {
-				binaryMismatch(s);
-			} else {
-				fprintf(stderr,
-				    "delta: can't figure out '%.*s'\n", len, b);
-			}
+			fprintf(stderr,
+			    "delta: can't figure out '%.*s'\n", len, b);
 			if (state) free(state);
 			if (slist) free(slist);
 			return (-1);
