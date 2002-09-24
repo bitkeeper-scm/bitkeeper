e0ffaa170c95663c12e4346836ddfaedfab1b076
Andrew Chang <awc@bitkeeper.com>
2002-09-24 13:18:54 -0700

    win32/import.sh: cannot use cygwin "kill" to send signal to non-cygwin      process such as "bk lock -w". replace "kill" with "bk unlock -w".
    bk: 3d90c8aedKfB9747xEy5iuEgfUYtDA

--- src/import.sh
+++ src/import.sh
@@ -722,7 +722,11 @@ Done() {
 	do	rm -f ${TMP}${i}$$
 	done
 	test X$LOCKPID != X && {
-		kill $LOCKPID || exit 100
+		# Win32 note: Do not use cygwin "kill" to kill a non-cygwin
+		# process such as "bk lock -w". It does not work,
+		# you will get "Operation not permitted".
+		bk unlock -w 
+		wait $LOCKPID # for win32
 		trap '' 0 2 3 15
 	}
 	exit $1
