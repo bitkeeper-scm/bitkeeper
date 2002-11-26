60cf54a86839c08ccb5da1cdf20107204dbf5fd3
Wayne Scott <wscott@bitkeeper.com>
2002-11-26 14:53:40 -0500

    Fix hole in our system() emulation.  We need to close stray filehandles before exec'ing subprograms.  (the bourne shell did this for us before)
    BUGID: 2002-11-25-002
    
    bk: 3de3d144aW83a5XY-5s5Gy8_0eDFcg

--- src/t/doit.sh
+++ src/t/doit.sh
@@ -28,14 +28,11 @@ win32_common_setup()
 	if [ X$BK_USER = XAdministrator ]; then BK_USER=Administrator-test; fi
 	if [ X$BK_USER = Xadministrator ]; then BK_USER=administrator-test; fi
 	USER="$BK_USER"	# some regression test uses $USER
+	export BK_USER
 
 	# don't run remote regressions on NT
 	if [ -z "$DO_REMOTE" ]; then DO_REMOTE=NO; fi
 	export DO_REMOTE
-
-	# turn off pager - needed in win98
-	PAGER=cat
-	export PAGER BK_USER
 }
 
 unix_common_setup()
@@ -200,6 +197,9 @@ setup_env()
 	check_w
 	chech_enclosing_repo
 
+	# turn off pager
+	BK_PAGER=cat
+	export BK_PAGER
 
 	unset BK_BIN _BK_GMODE_DEBUG
 	BK_LICENSE=ACCEPTED
