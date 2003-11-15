734680e6ab5891f801197e02349da4e02669212b
Larry McVoy <lm@bitkeeper.com>
2003-11-15 09:46:46 -0800

    Changes to not rewrite the file when we are compressing and we're already compressed.
    bk: 3fb66686dyvLidZVA95X0tpRVNGcww

--- src/slib.c
+++ src/slib.c
@@ -10735,7 +10735,13 @@ sccs_admin(sccs *sc, delta *p, u32 flags, char *new_encp, char *new_compp,
 	new_enc = sccs_encoding(sc, new_encp, new_compp);
 	if (new_enc == -1) return (-1);
 	unless ((flags & ADMIN_FORCE) || CSET(sc) || bkcl(0)) {
-		new_enc |= E_GZIP;
+		static	int nocompress = -1;
+
+		/* this is for the test suite only */
+		if (nocompress == -1) nocompress = getenv("BK_NOCOMPRESS") != 0;
+		unless (nocompress && (sc->nextserial < 10)) {
+			new_enc |= E_GZIP;
+		}
 	}
 	debug((stderr, "new_enc is %d\n", new_enc));
 	GOODSCCS(sc);
@@ -10790,9 +10796,7 @@ out:
 		goto out;
 	}
 
-	if (addSym("admin", sc, flags, s, &error)) {
-		flags |= NEWCKSUM;
-	}
+	if (addSym("admin", sc, flags, s, &error)) flags |= NEWCKSUM;
 	if (mode) {
 		delta *n = sccs_getrev(sc, "+", 0, 0);
 		mode_t m;
@@ -10830,10 +10834,10 @@ skipmode:
 
 	if (text) {
 		FILE	*desc;
-		char	dbuf[200];
 		char	*c;
+		char	dbuf[200];
 
-		if (!text[0]) {
+		unless (text[0]) {
 			if (sc->text) {
 				freeLines(sc->text, free);
 				sc->text = 0;
@@ -10847,7 +10851,7 @@ skipmode:
 			goto user;
 		}
 		desc = fopen(text, "rt"); /* must be text mode */
-		if (!desc) {
+		unless (desc) {
 			fprintf(stderr, "admin: can't open %s\n", text);
 			error = 1; sc->state |= S_WARNED;
 			goto user;
@@ -10976,17 +10980,21 @@ user:	for (i = 0; u && u[i].flags; ++i) {
 			}
 		}
 	}
-	if (flagsChanged) flags |= NEWCKSUM;
+	if (flagsChanged || (sc->encoding != new_enc)) flags |= NEWCKSUM;
 
 	if (flags & ADMIN_ADD1_0) {
 		insert_1_0(sc);
+		flags |= NEWCKSUM;
 	} else if (flags & ADMIN_RM1_0) {
-		unless (remove_1_0(sc)) flags &= ~ADMIN_RM1_0;
+		if (remove_1_0(sc)) {
+			flags |= NEWCKSUM;
+		} else {
+			flags &= ~ADMIN_RM1_0;
+		}
 	}
 
-	if ((flags & NEWCKSUM) == 0) {
-		goto out;
-	}
+	unless (flags & NEWCKSUM) goto out;
+
 	if (flags & ADMIN_OBSCURE) obscure_comments(sc);
 
 	/*
