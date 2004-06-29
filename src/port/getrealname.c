0a0b5d87615134f728c60c145815eca7910200e3
Wayne Scott <wscott@bitkeeper.com>
2004-06-29 17:29:04 -0500

    Fix performance problem on Windows in getRealName() by using GetLongPathName(). Includes a header from the Windows SDK to make sure the function exists on all platforms.  On unix the code is unchanged.
    bk: 40e1ed30XVf5uLzFfvuN6NUsugMNcA

--- src/port/getrealname.c
+++ src/port/getrealname.c
@@ -47,62 +47,6 @@ done:	if (realname[0] == 0) strcpy(realname, name);
 	return (0); /* ok */
 
 }
-#else
-private char *
-shortName(char *path, char *buf)
-{
-        if (GetShortPathName(path, buf, MAXPATH) == 0) {
-                /* cannot convert, assume "path" is real name */
-                strcpy(buf, path);
-        }
-        return (buf);
-}
-
-private int
-samePath(char *a, char *b)
-{
-        char    buf1[MAXPATH], buf2[MAXPATH];
-
-        return (strcasecmp(shortName(a, buf1), shortName(b, buf2)) == 0);
-}
-private int
-scanDir(char *dir, char *name, MDBM *db, char *realname)
-{
-        struct  _finddata_t found_file;
-        long    dh;
-        char    *file = found_file.name;
-        char    buf1[MAXPATH], buf2[MAXPATH];
-
-	realname[0] = 0;
-        bm2ntfname(dir, buf1);
-        strcat(buf1, "\\*.*");
-        if ((dh =  _findfirst(buf1, &found_file)) == -1L) goto done;
-
-        do {
-		if (streq(file, ".") || streq(file, "..")) continue;
-		sprintf(buf1, "%s/%s", dir, file);
-		sprintf(buf2, "%s/%s", dir, name);
-		if (db) mdbm_store_str(db, buf1, file, MDBM_INSERT);
-		if (samePath(buf1, buf2)) {
-			strcpy(realname, file);
-			break;
-		}
-        } while (_findnext(dh, &found_file) == 0);
-        _findclose(dh);
-
-	/*
-	 * If the entry does not exist (directory/file not created yet)
-	 * then the given name is the real name.
-	 */
-done:	if (realname[0] == 0) {
-		strcpy(realname, name);
-		sprintf(buf1, "%s/%s", dir, name);
-		if (db) mdbm_store_str(db, buf1, name, MDBM_INSERT);
-	}
-	return (0); /* ok */
-	
-}
-#endif
 
 /*
  * Given a path, find the real name of the base part
@@ -161,22 +105,8 @@ getRealName(char *path, MDBM *db, char *realname)
 	q = mypath;
 	r = realname;
 	
-#ifdef WIN32 /* dos colon handling */
-	if (q[1] == ':') {
-		/*
-		 * XXX This will break if we are on a different drive
-		 */
-		q[0] = toupper(q[0]); /* store drive letter in upper case */
-		assert(!islower(q[0]));
-		q = &q[3];
-		if (*q == 0) {
-			strcpy(realname, mypath);
-			return (0);
-	}
-#else
 	if (q[0] == '/') {
 		q = &q[1];
-#endif
 	} else {
 		q = mypath;
 		while (strneq(q, "../", 3)) {
@@ -239,3 +169,42 @@ getRealName(char *path, MDBM *db, char *realname)
 err:	fprintf(stderr, "getRealName failed: mypath=%s\n", mypath);
 	return (0);
 }
+#else
+
+/*
+ * We need the GetLongPathName function.  We use the NewAPIs.h header
+ * from the Windows SDK to include a emulated version on old win98. 
+ */
+
+#define	WANT_GETLONGPATHNAME_WRAPPER
+#define	COMPILE_NEWAPIS_STUBS
+
+#include "../win32/uwtlib/NewAPIs.h"
+
+/*
+ * File the official name for a file on the disk.  The name we pass in
+ * might be a short name, or it might have the wrong case.  The quick way
+ * to do this on Windows is to convert to the short name and then back
+ * to the long name.
+ */
+int
+getRealName(char *path, MDBM *db, char *realname)
+{
+	char	*p;
+	char	buf[MAXPATH];
+
+	unless (GetShortPathName(path, buf, MAXPATH)) strcpy(buf, path);
+	unless (GetLongPathName(buf, realname, MAXPATH)) strcpy(realname, buf);
+
+	/* GetLongPathName puts a / at the end of directories. */
+	p = realname + strlen(realname);
+	if ((p > realname) && (p[-1] == '/')) p[-1] = 0;
+
+#if	0
+	fprintf(stderr, "GetLong '%s' => '%s' => '%s'\n", path, buf, realname);
+#endif
+
+	return (0);
+}
+#endif
+
