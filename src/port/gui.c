dd66ddf5fdbbb91f7f8ede5fe26584e87d79f71b
Larry McVoy <lm@bitkeeper.com>
2005-03-31 15:33:00 -0800

    GUI prompts now happen only if bk prompt is called from the GUIs.
    bk: 424c88acCHBeEEZs_c3OG2LkYhts9A

--- src/port/gui.c
+++ src/port/gui.c
@@ -9,17 +9,7 @@ int
 gui_useDisplay()
 {
 	if (getenv("BK_NO_GUI_PROMPT")) return (0);
-
-	/* If we're windows and in a non-local bkd then no GUI */
-	if (win32() && getenv("_BK_IN_BKD") && !getenv("_BK_BKD_IS_LOCAL")) {
-		return (0);
-	}
-
-	/* FYI: Cygwin has X11 clients so there may be a display there too */
-	if (getenv("DISPLAY")) return (1);
-
-	/* Under Windows we always have a GUI at this point but not on Unix */
-	return (win32());
+	return (getenv("BK_GUI") != (char*)0);
 }
 
 char *
