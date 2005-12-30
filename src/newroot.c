432d58fdcf611c4922807e2eac094848142c5c3e
Rick Smith <rick@bitkeeper.com>
2005-12-30 11:26:27 -0800

    Loosen an internal undocumented interface in newroot to handle the case in csetprune where randomBits() returns fewer than 16 characters. The fixes regressions randomly failing with low frequency. This does not alter the public interface.
    bk: 43b589e3-gllWzIjLWXn8ZuJ1qXqUw

--- src/newroot.c
+++ src/newroot.c
@@ -36,9 +36,9 @@ newroot_main(int ac, char **av)
 	}
 	if (ranbits) {
 		u8	*p;
-		if (strlen(ranbits) != 16) {
+		if (strlen(ranbits) > 16) {
 k_err:			fprintf(stderr,
-			    "ERROR: -k option must have 16 lower case "
+			    "ERROR: -k option can have at most 16 lower case "
 			    "hex digits\n");
 			goto usage;
 		}
