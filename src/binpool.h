762077f53c3c1abc531edae5d761efc90aebc27d
Larry McVoy <lm@bitkeeper.com>
2007-03-12 09:48:01 -0700

    Remove the bpattr arg to bp_lookupkeys(), a local works fine.
    bk: 45f58441bmFNmWD7YIc3mLkroyGfSA

--- src/binpool.h
+++ src/binpool.h
@@ -10,6 +10,6 @@ typedef struct {
 int	bp_loadAttr(char *file, bpattr *a);
 int	bp_saveAttr(bpattr *a, char *file);
 void	bp_freeAttr(bpattr *a);
-char	*bp_lookupkeys(project *p, char *hash, char *keys, bpattr *a);
+char	*bp_lookupkeys(project *p, char *hash, char *keys);
 int	bp_insert(project *p, char *file, char *hash, char *keys, int canmv);
 
