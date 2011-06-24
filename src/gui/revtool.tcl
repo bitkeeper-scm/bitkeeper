212ead8b96d20fae6bc221e02e643e8038211d04
Damon Courtney <damon@bitkeeper.com>
2011-06-23 19:12:12 -0500

    Fix a bug in subline highlighting when we have no line to highlight.
    bk: 4e03d65cArxVu0Ce9yqXyfa_QxP_ww

--- src/gui/revtool.tcl
+++ src/gui/revtool.tcl
@@ -1775,8 +1775,10 @@ proc diffs {diffs l} \
 	}
 	catch { close $diffs; }
 
-	set x [string first "|" $line]
-	highlightStacked $w(aptext) 1.0 end [incr x]
+	if {[info exists line]} {
+		set x [string first "|" $line]
+		highlightStacked $w(aptext) 1.0 end [incr x]
+	}
 }
 
 proc done {} \
