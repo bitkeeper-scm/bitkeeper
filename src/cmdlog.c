3a4cd4408bd07316bc294692704ed128abdca134
Georg Nikodym <georgn@bitkeeper.com>
2012-11-28 18:19:19 -0500

    Make cmdlog nested aware by having it collate logs the product as well as all the components. Add -S for standalone like everywhere else.
    bk: 50b69bf74w5YAYZOeZJ0Yx08--xgiA

--- src/cmdlog.c
+++ src/cmdlog.c
@@ -2,155 +2,270 @@
 #include "sccs.h"
 #include "range.h"
 
+typedef struct {
+	u32	all:1;	      /* -a: show cmd_log instead of repo_log */
+	u32	verbose:1;    /* -v: print raw logfile */
+	u32	standalone:1; /* -S: standalone */
+
+	time_t	cutoff;       /* -c: cutoff date */
+	char	*pattern;
+	search	search;
+	int	complen;      /* max component pathname (for -N) */
+} Opts;
+
+typedef struct {
+	char	*prefix;	/* component owning log */
+	FILE	*fp;		/* open to comp's cmd_log file */
+	char	*buf;		/* next line from logfile */
+	time_t	ts;		/* timestamp on this line */
+} logmux;
+
+private void	parsePrint(Opts *opts, char *prefix, char *buf);
+private time_t	parseTimestamp(char *buf);
+private int	logmux_oldest(char **lmlist);
+private void	logmux_free(void *lmp);
+
 int
 cmdlog_main(int ac, char **av)
 {
+	Opts	*opts;
 	FILE	*f;
+	int	c, i, nested;
+	char	*t, *log;
+	logmux	*lmp;
+	char	**lmlist = 0;
+	char	buf[MAXPATH];
+	longopt	lopts[] = {
+		{ "standalone", 'S' },
+		{ 0, 0 }
+	};
+
+	opts = new(Opts);
+	while ((c = getopt(ac, av, "Sac;v", lopts)) != -1) {
+		switch (c) {
+		    case 'S': opts->standalone = 1; break;
+		    case 'a': opts->all = 1; break;
+		    case 'c': opts->cutoff = range_cutoff(optarg + 1); break;
+		    case 'v': opts->verbose = 1; break;
+		    default: bk_badArg(c, av);
+		}
+	}
+	nested = bk_nested2root(opts->standalone);
+	if (av[optind]) {
+		if (av[optind+1]) usage();
+		if (av[optind][0] != '^' && !opts->all) {
+			opts->pattern = aprintf("^%s/", av[optind]);
+		} else {
+			opts->pattern = aprintf("%s/", av[optind]);
+		}
+		opts->search = search_parse(opts->pattern);
+	}
+
+	log = opts->all ? "cmd_log" : "repo_log";
+
+	/* standalone case */
+	unless (nested) {
+		concat_path(buf, proj_root(0), "/BitKeeper/log/");
+		concat_path(buf, buf, log);
+		unless (f = fopen(buf, "r")) return (1);
+		while (t = fgetline(f)) parsePrint(opts, 0, t);
+		fclose(f);
+		if (opts->pattern) free(opts->pattern);
+		return (0);
+	}
+
+	f = popen("bk comps -ch", "r");
+	while (t = fgetline(f)) {
+		lmp = new(logmux);
+		lmp->prefix = strdup(t);
+		if (strlen(t) > opts->complen) opts->complen = strlen(t);
+		sprintf(buf, "%s/BitKeeper/log/%s", lmp->prefix, log);
+		unless (exists(buf)) {
+			lmp->fp = 0;
+			logmux_free(lmp);
+			continue;
+		}
+		unless (lmp->fp = fopen(buf, "r")) {
+			perror("fopen");
+			return (1);
+		}
+		/* grab the first line, if any */
+		unless (lmp->buf = fgetline(lmp->fp)) {
+			logmux_free(lmp);
+			continue;
+		}
+		lmp->ts = parseTimestamp(lmp->buf);
+		lmlist = addLine(lmlist, lmp);
+	}
+	pclose(f);
+
+	while (i = logmux_oldest(lmlist)) {
+		lmp = (logmux *)lmlist[i];
+		parsePrint(opts, lmp->prefix, lmp->buf);
+		unless (lmp->buf = fgetline(lmp->fp)) {
+			removeLineN(lmlist, i, logmux_free);
+			continue;
+		}
+		lmp->ts = parseTimestamp(lmp->buf);
+	}
+	free(opts);
+	return (0);
+}
+
+private void
+parsePrint(Opts *opts, char *prefix, char *buf) {
 	time_t	t;
 	char	*p, *p1, *equals;
 	char	*version;
 	char	*user;
-	char	buf[MAXPATH*3];
-	int	yelled = 0, c, ind;
-	struct	{
-		u32	all:1;	   /* -a: show cmd_log instead of repo_log */
-		u32	verbose:1; /* -v: print raw logfile */
+	int	yelled = 0, ind;
 
-		time_t	cutoff;    /* -c: cutoff date */
-		char	*pattern;
-		search	search;
-	} opts;
-
-	unless (proj_root(0)) return (1);
-	memset(&opts, 0, sizeof(opts));
-	while ((c = getopt(ac, av, "ac;v", 0)) != -1) {
-		switch (c) {
-		    case 'a': opts.all = 1; break;
-		    case 'c': opts.cutoff = range_cutoff(optarg + 1); break;
-		    case 'v': opts.verbose = 1; break;
-		    default: bk_badArg(c, av);
-		}
-	}
-	if (av[optind]) {
-		if (av[optind][0] != '^' && !opts.all) {
-			opts.pattern = aprintf("^%s/", av[optind]);
-		} else {
-			opts.pattern = aprintf("%s/", av[optind]);
-		}
-		opts.search = search_parse(opts.pattern);
-	}
-	concat_path(buf, proj_root(0), "/BitKeeper/log/");
-	concat_path(buf, buf, (opts.all ? "cmd_log" : "repo_log"));
-	f = fopen(buf, "r");
-	unless (f) return (1);
-	while (fgets(buf, sizeof(buf), f)) {
-		user = buf;
-		for (p = log_versions; *p; ++p) {
-			if (*p == buf[0]) {
-				if ((p-log_versions) > LOGVER) {
-					if (yelled) goto nextline;
-					printf("cannot display this "
-					       "log entry; please upgrade\n");
-					yelled = 1;
-					goto nextline;
-				}
-				user = buf+1;
-				break;
+	user = buf;
+	for (p = log_versions; *p; ++p) {
+		if (*p == buf[0]) {
+			if ((p-log_versions) > LOGVER) {
+				if (yelled) return;
+				printf("cannot display this "
+				    "log entry; please upgrade\n");
+				yelled = 1;
+				return;
 			}
+			user = buf+1;
+			break;
 		}
+	}
 
-		for (p = user; (*p != ' ') && (*p != '@'); p++);
-		*p++ = 0;
-		t = strtoul(p, &version, 0);
-		while (isspace(*version)) ++version;
-		if (*version == ':') {
-			p = version;
-			*p = 0;
+	for (p = user; (*p != ' ') && (*p != '@'); p++);
+	*p++ = 0;
+	t = strtoul(p, &version, 0);
+	while (isspace(*version)) ++version;
+	if (*version == ':') {
+		p = version;
+		*p = 0;
+		version = 0;
+	} else {
+		char *q;
+
+		unless (p = strchr(p, ':')) return;
+		*p = 0;
+
+		unless (isalnum(*version)) {
 			version = 0;
-		} else {
-			char *q;
-
-			unless (p = strchr(p, ':')) continue;
-			*p = 0;
-
-			unless (isalnum(*version)) {
-				version = 0;
-			} else  {
-				for (q = 1+version; *q; ++q) {
-					if ( (*q & 0x80) || (*q < ' ') ) {
-						version = 0;
-						break;
-					}
+		} else  {
+			for (q = 1+version; *q; ++q) {
+				if ( (*q & 0x80) || (*q < ' ') ) {
+					version = 0;
+					break;
 				}
 			}
 		}
-		unless (t >= opts.cutoff) continue;
-		p+=2; // skip colon and extra space inserted by log indenting
-		chomp(p);
-		/* find indent, if any */
-		for (p1 = p; isspace(*p1); p1++);
-		ind = p1 - p;
-
-		/*
-		 * if we got a pattern, do pattern matching
-		 * skip this log entry on no match
-		 */
-		if (opts.pattern) {
-			unless (search_either(p, opts.search)) continue;
-			/*
-			 * if somebody is looking for foo, then
-			 * they'll also match the "bk cmdlog foo" so
-			 * don't print ourselves, unless we are
-			 * actually looking for cmdlog entries
-			 */
-			unless (strneq(opts.pattern, "cmdlog", 6) ||
-			    strneq(opts.pattern, "^cmdlog", 7)) {
-				if (strneq(p, "cmdlog ", 7)) continue;
-			}
-		}
-
-		/*
-		 * - the presence of a indent means this was not a user
-		 *   run command.
-		 * - if there is no equal sign it's not a command completion
-		 * - if we have no verbose flag, we skip all but entries
-		 *   for completed user commands
-		 */
-		equals = strstr(p, " = ");
-		if (!opts.verbose && (!equals || ind)) continue;
-
-		/* output requested annotations */
-		if (opts.verbose) {
-			printf("%s %.19s %14s ",
-			    user, ctime(&t), version ? version : "");
-		}
-		if (equals) *equals = 0;
-
-		/*
-		 * Output command.  If opts.verbose is set, do
-		 * not prefix with "bk ".
-		 * Note: the ind+1 is to balance the cmd_log being
-		 * written with printf("%*s", x, " "); which
-		 * prints out 1 space for x = 0, otherwise it prints x
-		 * spaces.  Above (the +=2) we eat one space
-		 * corresponding to x = 0, so x = 4 means 3 spaces,
-		 * and x = 8 means 7 spaces.  So we're unmathing it and
-		 * getting it to look regular.
-		 */
-		if (ind) printf("%*s", ind+1, " ");
-		if (!opts.verbose && !strneq(p1, "bk ", 3)) {
-			fputs("bk ", stdout);
-		}
-		fputs(p1, stdout);
-		if (equals && opts.verbose) {
-			*equals = ' ';
-			printf("%s", equals);
-		}
-		printf("\n");
-
-nextline:	;
 	}
-	fclose(f);
-	if (opts.pattern) free(opts.pattern);
-	return (0);
+	unless (t >= opts->cutoff) return;
+	p+=2; // skip colon and extra space inserted by log indenting
+	chomp(p);
+	/* find indent, if any */
+	for (p1 = p; isspace(*p1); p1++);
+	ind = p1 - p;
+
+	/*
+	 * if we got a pattern, do pattern matching
+	 * skip this log entry on no match
+	 */
+	if (opts->pattern) {
+		unless (search_either(p, opts->search)) return;
+		/*
+		 * if somebody is looking for foo, then
+		 * they'll also match the "bk cmdlog foo" so
+		 * don't print ourselves, unless we are
+		 * actually looking for cmdlog entries
+		 */
+		unless (strneq(opts->pattern, "cmdlog", 6) ||
+		    strneq(opts->pattern, "^cmdlog", 7)) {
+			if (strneq(p, "cmdlog ", 7)) return;
+		}
+	}
+
+	/*
+	 * - the presence of a indent means this was not a user
+	 *   run command.
+	 * - if there is no equal sign it's not a command completion
+	 * - if we have no verbose flag, we skip all but entries
+	 *   for completed user commands
+	 */
+	equals = strstr(p, " = ");
+	if (!opts->verbose && (!equals || ind)) return;
+
+	/* output requested annotations */
+	if (opts->verbose) {
+		printf("%-8s %.19s %14s ",
+		    user, ctime(&t), version ? version : "");
+		if (prefix) printf("[%-*s] ", opts->complen, prefix);
+	}
+	if (equals) *equals = 0;
+
+	/*
+	 * Output command.  If opts->verbose is set, do
+	 * not prefix with "bk ".
+	 * Note: the ind+1 is to balance the cmd_log being
+	 * written with printf("%*s", x, " "); which
+	 * prints out 1 space for x = 0, otherwise it prints x
+	 * spaces.  Above (the +=2) we eat one space
+	 * corresponding to x = 0, so x = 4 means 3 spaces,
+	 * and x = 8 means 7 spaces.  So we're unmathing it and
+	 * getting it to look regular.
+	 */
+	if (ind) printf("%*s", ind+1, " ");
+	if (!opts->verbose && !strneq(p1, "bk ", 3)) {
+		fputs("bk ", stdout);
+	}
+	fputs(p1, stdout);
+	if (equals && opts->verbose) {
+		*equals = ' ';
+		printf("%s", equals);
+	}
+	printf("\n");
+}
+
+private time_t
+parseTimestamp(char *buf)
+{
+	char	*p;
+
+	for (p = buf; *p != ' '; p++);
+	p++;
+	return (strtoul(p, 0, 0));
+}
+
+/*
+ * Pick the nested cmd_log line to print from an array of the next line
+ * from each component.
+ * Lines are sorted by time, and then by comp pathname if the time is
+ * identical.
+ */
+private int
+logmux_oldest(char **lmlist)
+{
+	int	i, ret = 0;
+	logmux	*lmp, *oldest = 0;
+
+	EACH_STRUCT(lmlist, lmp, i) {
+		if (!oldest ||
+		    ((lmp->ts == oldest->ts)
+			? (strcmp(lmp->prefix, oldest->prefix) < 0)
+			: (lmp->ts < oldest->ts))) {
+			oldest = lmp;
+			ret = i;
+		}
+	}
+	return (ret);
+}
+
+private void
+logmux_free(void *lmp)
+{
+	logmux	*p = lmp;
+
+	free(p->prefix);
+	if (p->fp) fclose(p->fp);
+	free(p);
 }
