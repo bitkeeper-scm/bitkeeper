f35901d545ea451569167ff2a37835d0ecd75176
Rob Netzer <rob@bitkeeper.com>
2013-08-05 13:39:07 -0400

    L: update L's getopt from bk's latest.
    bk: 51ffe33bAnvZh1nbPt1DMvaEJVdy2g

--- src/gui/tcltk/tcl/generic/Lgetopt.c
+++ src/gui/tcltk/tcl/generic/Lgetopt.c
@@ -3,11 +3,10 @@
 /* for compat with code below */
 #define streq(a, b)	(!strcmp(a, b))
 #define strneq(a, b, n)	(!strncmp(a, b, n))
-#define debug(x)
 #define assert(x)
 
 /*
- * Copyright (c) 1997-2010 BitMover, Inc.
+ * Copyright (c) 1997 L.W.McVoy
  *
  * This version handles
  *
@@ -21,6 +20,7 @@
  *	-abcr<arg>
  *	-r<arg> -R<arg>, etc.
  *	--long
+ *	--long:<arg>
  *	--long=<arg>
  *	--long <arg>
  *
@@ -40,7 +40,8 @@
 int	optopt;		/* option that is in error, if we return an error */
 int	optind;		/* next arg in argv we process */
 char	*optarg;	/* argument to an option */
-static int n;
+static int n;		/* current position == av[optind][n] */
+static int lastn;	/* saved copy of last n */
 
 private	int	doLong(int ac, char **av, longopt *lopts);
 
@@ -49,6 +50,15 @@ getoptReset(void)
 {
 	optopt = optind = 0;
 	optarg = 0;
+}
+
+void
+getoptConsumed(int n1)
+{
+	optind--;
+	unless (optind) optind = 1;
+	n = lastn + n1;
+	// TRACE("optind = %d, n = %d, n1 = %d", optind, n, n1);
 }
 
 /*
@@ -62,12 +72,14 @@ getopt(int ac, char **av, char *opts, longopt *lopts)
 {
 	char	*t;
 
+	optarg = 0;	/* clear out arg from last round */
+	optopt = 0;	/* clear error return */
 	if (!optind) {
 		optind = 1;
+		lastn = n;
 		n = 1;
 	}
-	debug((stderr, "GETOPT ind=%d n=%d arg=%s av[%d]='%s'\n",
-	    optind, n, optarg ? optarg : "", optind, av[optind]));
+	// TRACE("GETOPT ind=%d n=%d av[%d]='%s'", optind, n, optind, av[optind]);
 
 	if ((optind >= ac) || (av[optind][0] != '-') || !av[optind][1]) {
 		return (GETOPT_EOF);
@@ -75,6 +87,7 @@ getopt(int ac, char **av, char *opts, longopt *lopts)
 	/* Stop processing options at a -- and return arguments after */
 	if (streq(av[optind], "--")) {
 		optind++;
+		lastn = n;
 		n = 1;
 		return (GETOPT_EOF);
 	}
@@ -88,7 +101,8 @@ getopt(int ac, char **av, char *opts, longopt *lopts)
 	}
 	if (!*t) {
 		optopt = av[optind][n];
-		debug((stderr, "\tran out of option letters\n"));
+		// TRACE("%s", "ran out of option letters");
+		lastn = n;
 		if (av[optind][n+1]) {
 			n++;
 		} else {
@@ -102,57 +116,55 @@ getopt(int ac, char **av, char *opts, longopt *lopts)
 	 * If it isn't one that takes an option, just advance and return.
 	 */
 	if (t[1] != ':' && t[1] != '|' && t[1] != ';') {
+		lastn = n;
 		if (!av[optind][n+1]) {
 			optind++;
 			n = 1;
 		} else {
 			n++;
 		}
-		debug((stderr, "\tLegit singleton %c\n", *t));
+		// TRACE("Legit singleton %c", *t);
 		return (*t);
 	}
 
 	/* got one with an option, see if it is cozied up to the flag */
 	if (av[optind][n+1]) {
-		if (av[optind][n+1]) {
-			optarg = &av[optind][n+1];
-		} else {
-			optarg = 0;
-		}
+		optarg = &av[optind][n+1];
 		optind++;
+		lastn = n;
 		n = 1;
-		debug((stderr, "\t%c with %s\n", *t, optarg));
+		// TRACE("%c with %s", *t, optarg);
 		return (*t);
 	}
 
 	/* If it was not there, and it is optional, OK */
 	if (t[1] == '|') {
-		optarg = 0;
 		optind++;
+		lastn = n;
 		n = 1;
-		debug((stderr, "\t%c without arg\n", *t));
+		// TRACE("%c without arg", *t);
 		return (*t);
 	}
 
 	/* was it supposed to be there? */
 	if (t[1] == ';') {
-		optarg = 0;
 		optind++;
 		optopt = *t;
-		debug((stderr, "\twanted another word\n"));
+		// TRACE("%s", "wanted another word");
 		return (GETOPT_ERR);
 	}
 
 	/* Nope, there had better be another word. */
 	if ((optind + 1 == ac) || (av[optind+1][0] == '-')) {
 		optopt = av[optind][n];
-		debug((stderr, "\twanted another word\n"));
+		// TRACE("%s", "wanted another word");
 		return (GETOPT_ERR);
 	}
 	optarg = av[optind+1];
 	optind += 2;
+	lastn = n;
 	n = 1;
-	debug((stderr, "\t%c with arg %s\n", *t, optarg));
+	// TRACE("%c with arg %s", *t, optarg);
 	return (*t);
 }
 
@@ -164,15 +176,15 @@ doLong(int ac, char **av, longopt *lopts)
 
 	unless (lopts) {
 err:		n = 1;
-		optarg = 0;
 		optind++;
 		optopt = 0;
 		return (GETOPT_ERR);
 	}
 	/* len of option without =value part */
 	s = av[optind] + 2;
-	len1 = (t = strchr(s, '=')) ? (t - s) : strlen(s);
-	for (; t = lopts->name; lopts++) {
+	unless (t = strchr(s, '=')) t = strchr(s, ':');
+	len1 = t ? (t - s) : strlen(s);
+	for (; (t = lopts->name); lopts++) {
 		s = av[optind] + 2;
 		/* len of lopts array without suffix */
 		len2 = strlen(t);
@@ -190,14 +202,15 @@ err:		n = 1;
 	 * If it isn't one that takes an option, just advance and return.
 	 */
 	unless (*t) {
-		if (*s == '=') goto err; /* got option anyway */
+		/* got option anyway */
+		if ((*s == '=') || (*s == ':')) goto err;
 		optind++;
 		n = 1;
 		return (lopts->ret);
 	}
 
 	/* got one with an option, see if it is cozied up to the flag */
-	if (*s == '=') {
+	if ((*s == '=') || (*s == ':')) {
 		optarg = s + 1;
 		optind++;
 		n = 1;
@@ -206,7 +219,6 @@ err:		n = 1;
 
 	/* If it was not there, and it is optional, OK */
 	if (*t == '|') {
-		optarg = 0;
 		optind++;
 		n = 1;
 		return (lopts->ret);
