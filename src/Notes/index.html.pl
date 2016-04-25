6fc1deddabeda6822168e6cda971e034b6d19a66
Wayne Scott <wscott@bitkeeper.com>
2016-04-25 17:09:39 -0400

    Tweak the formating of the Notes directory a bit.
    bk: 571e8793cOk51b3q27tC7_kBYq7EBw

--- src/Notes/index.html.pl
+++ src/Notes/index.html.pl
@@ -1,13 +1,48 @@
 #!/usr/bin/perl -w
 
-print "<html><body><ul>\n";
+print <<EOF;
+<!DOCTYPE html>
+<html>
+<head>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+<script type="text/javascript" src="http://cdn.ucb.org.br/Scripts/tablesorter/jquery.tablesorter.min.js"></script>
+</head>
+<body>
+<table id="myTable" class="tablesorter">
+<thead>
+<tr>
+    <th>Name</th>
+    <th>Description</th>
+    <th>Last Modified</th>
+</tr>
+</thead>
+<tbody>
+
+EOF
+
 foreach my $f (@ARGV) {
     ($_ = $f) =~ s/.adoc//;
-    print "<li><A href=\"$_.html\">$_</A> - ";
+    print "<tr>\n";
+    print "\t<td><A href=\"$_.html\">$_</A></td>\n";
+
     open(F, $f);
     $_ = <F>;
     close(F);
-    print $_;
-    print "</LI>\n";
+    print "\t<td>$_</td>\n";
+    printf "\t<td>%s</td>\n", `bk prs -hnd':D: :T:' $f`;
+    print "</tr>\n";
 }
-print "</ul></body></html>\n";
+
+print <<'EOF'
+</tbody>
+</table>
+<script>
+$(document).ready(function()
+    {
+        $("#myTable").tablesorter();
+    }
+);
+</script>
+</body>
+</html>
+EOF
